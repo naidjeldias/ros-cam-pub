f924bd2499d3d7332825ad61f9f568977554109f
Nigel Dias <nigeldias2012@gmail.com>
2018-10-13 00:02:46 -0300

    Configuração de câmera

--- src/cam_pub_node.cpp
+++ src/cam_pub_node.cpp
@@ -2,45 +2,41 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <ros/ros.h>
+#include <ros/package.h>
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include "vcap/vcap.hpp"
 #include "vcap/device.hpp"
+#include <cstdio>
+#include "json/json.hpp"
 
 namespace enc = sensor_msgs::image_encodings;
 using namespace std;
 using namespace cv;
+using json = nlohmann::json;
+
+
+
 
 class CamCap{
+    private:
+        vcap::device vd;
+        json configs;
+
+    
     public:
         CamCap(){
-            vcap::device vd;
+            
             int w, h;
-            unsigned char *rgb = NULL;
-            //VideoCapture cap(0);
- 
-            if (!vd.open_device("/dev/video0")) {
-                std::cout << "Device 0 not openned...\n";
-            }
-
-            if( !vd.set_frame_size(320,240) ) {
-                printf("Frame size is not avaliable!\n");
-            }
+            unsigned char * rgb = NULL;
+            int rate = 30;
+            
+            CamConfig();
 
             vd.get_image_dimension(&w, &h);
             rgb = (unsigned char *)malloc(sizeof(unsigned char) * w * h * 3);
-
-            /*
-            if(!cap.isOpened())
-            {
-                cout << "Error opening video stream or file" << endl;
-            }
-            */
-
-            //Mat frame;
-            int rate = 30;
 
             //create a node handle: it is reference assigned to a new node
             //every node created must has a reference to create publishers and subscibers
@@ -61,8 +57,6 @@ class CamCap{
                 vd.grabRGB(rgb);
 
                 Mat frame(h, w, CV_8UC3, rgb);
-
-                //cap >> frame;
 
                 if(frame.empty())
                     break;
@@ -85,26 +79,89 @@ class CamCap{
                 //Sleep for the rest of the cycle, to enforce the loop rate
                 loop_rate.sleep();
             }
-            vd.stop();
-
-            vd.close_device();
-
+            
             std::cout << "\n";
-
             free(rgb);
-            //cap.release();
 
         }
 
         ~CamCap(){
-            
-            destroyAllWindows();
+            vd.stop();
+            vd.close_device();
         }
+
+        void CamConfig(){
+
+            // enum control default
+            struct v4l2_queryctrl qctrl;
+
+            if (!vd.open_device("/dev/video1")) {
+                std::cout << "Device not openned...\n";
+            }
+
+            if( !vd.set_frame_size(320,240) ) {
+                std::cout << "Frame size is not avaliable!\n";
+            }
+    
+            if(!loadFIle()){
+                std::cout << "Can not open file\n";
+            }else{
+                auto configArray = configs["config"];
+                //std::cout << "Size" << configArray.size();
+                for(const auto &prop : configArray){
+                    //std::cout << "Name" << prop["name"];
+                    int type = prop["type"];
+                    int id = prop["id"];
+                    int value = prop["value"];
+         
+                    qctrl.id = id;                    
+                    qctrl.type = type;
+
+                    if(vcap::set_control(vd.get_fd(), qctrl.id, value) < 0)
+                        std::cout << "ERROR seting "<< prop["name"]<<"\n";
+                    /*
+                    switch(type){
+                        case V4L2_CTRL_TYPE_INTEGER:
+                            break;
+                        case V4L2_CTRL_TYPE_BOOLEAN:
+                            break;
+                        case V4L2_CTRL_TYPE_MENU:
+                            break;
+                        default:
+				            break;
+                    }
+                    */
+
+                }
+
+            }
+
+        }
+
+        bool loadFIle(){
+            try {
+                string file_path =  ros::package::getPath("cam_pub");
+                file_path = file_path+"/src/cam_config.json";
+                ifstream file(file_path);
+                if (file.is_open()) {
+                    file >> configs;
+                    file.close();
+                } else return false;
+            } catch (exception &e) {
+                string error = e.what();
+                if (error.find("parse_error") != string::npos) return 2;
+            }
+            return true;
+
+        }
+
+        
 
 };
 
 int main(int argc, char **argv)
 {
+    
     //Initialize new ROS node named "cam_node"
     ros::init(argc, argv, "cam_node");
     CamCap camcap;
